35fc69b6c73772684e39f95aa06e6e9b51af399d
Anthony Pogliano <poglianoster@gmail.com>
2025-03-05 16:58:34 -0700

    Updating to tailwind 4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import preact from '@astrojs/preact'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact({ compat: true })],
+  vite: { plugins: [tailwindcss()] },
+  integrations: [preact({ compat: true }), icon()],
   devToolbar: { enabled: false },
   markdown: {
     shikiConfig: {
