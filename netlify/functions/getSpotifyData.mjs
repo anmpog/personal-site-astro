49c84d5a130f7bf919ecb79649db4bbd064cdc3a
Anthony Pogliano <poglianoster@gmail.com>
2025-08-14 15:19:40 -0600

    test(netlify): Attempting to modify res headers
    I apparently cannot debug this stuff using the netlify CLI so I am
    pushing changes to staging to see if they show up in the staging preview

--- netlify/functions/getSpotifyData.mjs
+++ netlify/functions/getSpotifyData.mjs
@@ -37,6 +37,7 @@ export default async function getSpotifyData(req) {
 
   if (req.method === 'GET') {
     let accessToken = ''
+    console.log('Get request to getSpotifyData')
 
     try {
       const { data } = await axios.request(refreshTokenOptions)
@@ -44,7 +45,15 @@ export default async function getSpotifyData(req) {
 
       const getRecentTracksResponse = await getRecentTracks(accessToken)
       const getTopArtistsResponse = await getTopArtists(accessToken)
-      const cacheControlHeaders = new CacheHeaders(undefined, 'netlify')
+      const cacheControlHeaders = new CacheHeaders(
+        { 'Content-Type': 'application/json', 'My-header': 'testing' },
+        'netlify'
+      )
+
+      console.log('Cache control headers: ', cacheControlHeaders)
+      console.log('Testing structure of headers obj: ', {
+        ...cacheControlHeaders,
+      })
 
       const successResponse = new Response(
         JSON.stringify({
