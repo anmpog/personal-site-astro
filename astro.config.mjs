95e05d685cfd2cbefb139622969f1c598bf89dfb
Anthony Pogliano <poglianoster@gmail.com>
2025-08-25 14:46:17 -0600

    style(blog): Adding style changes for blog posts
    Various style changes for blog posts that I found necessary while
    authoring spotify-widget-blog post.
    
    Includes:
    - clearer interactivity cues for anchor tags
    - markdown-specific stylesheet
    - code-block styling from expressive-code instead of shiki
    - various minor style updates targeting markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,30 @@ import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
+import expressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
   // @ts-ignore
-  integrations: [tailwind(), preact({ compat: true }), icon()],
+  integrations: [
+    tailwind(),
+    preact({ compat: true }),
+    icon(),
+    expressiveCode({
+      // You can use any of the themes bundled with Shiki by name,
+      // specify a path to JSON theme file, or pass an instance
+      // of the `ExpressiveCodeTheme` class here:
+      themes: ['everforest-dark'],
+      shiki: {
+        // You can pass additional plugin options here,
+        // e.g. to load custom language grammars:
+        langs: [
+          // import('./some-exported-grammar.mjs'),
+          // JSON.parse(fs.readFileSync('./some-json-grammar.json', 'utf-8'))
+        ],
+      },
+    }),
+  ],
   devToolbar: { enabled: false },
   site: 'http://www.anmpog.dev',
-  markdown: {
-    shikiConfig: {
-      theme: 'everforest-dark',
-    },
-  },
 })
